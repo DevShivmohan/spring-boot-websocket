84113483c0ba20661ca43890dc75af6f0d4d9478
Shivmohan <nshivmohan08@gmail.com>
2024-09-09 23:20:26 +0530

    kafka tls config and dlt handler

--- src/main/java/shiv/web/socket/consumer/KafkaConsumer.java
+++ src/main/java/shiv/web/socket/consumer/KafkaConsumer.java
@@ -2,60 +2,87 @@ package shiv.web.socket.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
-import org.apache.kafka.common.errors.RetriableException;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
 public class KafkaConsumer {
-    private int count=0;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @RetryableTopic(
-            attempts = "5",
-            backoff = @Backoff(delay = 2000, multiplier = 2.0),
-            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE,
-            dltStrategy = DltStrategy.FAIL_ON_ERROR
-    )
-    @KafkaListener(topics = "create-order", groupId = "order-transactions")
-    public void listenWithRetry(String message, Acknowledgment ack) {
+//    @RetryableTopic(
+//            attempts = "5",
+//            backoff = @Backoff(delay = 10000),
+//            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE
+//    )
+//    @KafkaListener(topics = "create-order", groupId = "order-transactions")
+    public void listenWithRetry(@Payload String message,
+                                @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
         try {
-            log.info("Processing message: {}", message);
+            log.info("Processing message from partition {}: {}", partition, message);
 
             // Processing logic
             if (message.contains("Shiv")) {
                 throw new RuntimeException("Simulating a failure");
             }
 
-            ack.acknowledge();  // Acknowledge after successful processing
+//            acknowledgment.acknowledge();  // Acknowledge after successful processing
         } catch (Exception e) {
             log.error("Error encountered, retrying...", e);
             throw e;  // Retry within Spring Retry mechanism
         }
     }
 
-
-    @KafkaListener(topics = "create-order-dlq", groupId = "order-transactions-dlq")
-    public void listenToDlq(String message, Acknowledgment ack) {
+    @DltHandler
+//    @KafkaListener(topics = "create-order-dlq", groupId = "order-transactions-dlq")
+    public void listenToDlq(@Payload String message,@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partitio) {
         try {
-            log.info("Retrying message from DLQ: {}", message);
-            // Retry logic for failed messages
-            if(message.contains("Shiv")){
+            log.info("Processing message from DLQ: {}", message);
+
+            // DLQ Processing logic
+            // You can decide how to handle this message, e.g., logging, alerting, or forwarding to another topic
+            if (message.contains("Shiv")) {
+                log.error("Message still contains issue, won't be processed: {}", message);
                 throw new RuntimeException("DLQ runtime error");
             }
-            ack.acknowledge();  // Acknowledge after successful processing
 
+//            acknowledgment.acknowledge();  // Acknowledge after successful processing from DLQ
         } catch (Exception e) {
             log.error("Failed again, leaving in DLQ: {}", message, e);
-            // Optionally handle retries within DLQ as well
+            // Leave the message in DLQ for manual investigation or other handling
         }
     }
 
+
+//    @RetryableTopic(
+//            attempts = "5",
+//            backoff = @Backoff(delay = 10000),
+//            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE
+//    )
+//    @KafkaListener(topics = "create-order", groupId = "order-transactions")
+    public void listenWithRetry1(@Payload String message,
+                                @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        try {
+            log.info("Processing message1 from partition {}: {}", partition, message);
+
+            // Processing logic
+            if (message.contains("Mohan")) {
+                throw new RuntimeException("Simulating a failure");
+            }
+
+//            acknowledgment.acknowledge();  // Acknowledge after successful processing
+        } catch (Exception e) {
+            log.error("Error encountered, retrying...", e);
+            throw e;  // Retry within Spring Retry mechanism
+        }
+    }
 }
--- src/main/java/shiv/web/socket/producer/KafkaProducer.java
+++ src/main/java/shiv/web/socket/producer/KafkaProducer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -24,5 +25,13 @@ public class KafkaProducer {
         map.put("name", "Shivmohan");
         kafkaTemplate.send("create-order", value);
         log.info("Emitted kafka event {}", value);
+    }
+
+    @Scheduled(fixedRate = 1000L)
+    public void sendDummyMessages() throws JsonProcessingException {
+        final var uuid = UUID.randomUUID().toString();
+        log.info("sending message {}", uuid);
+        sendDummyMessages(uuid);
+        log.info("sent message {}", uuid);
     }
 }
