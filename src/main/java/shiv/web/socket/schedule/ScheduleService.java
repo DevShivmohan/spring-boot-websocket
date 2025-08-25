f5ff9e48256f2d0d7745384682879553e4216f74
Shivmohan <nshivmohan08@gmail.com>
2025-08-25 18:49:30 +0530

    test

---
+++ src/main/java/shiv/web/socket/schedule/ScheduleService.java
@@ -0,0 +1,51 @@
+package shiv.web.socket.schedule;
+
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Base64;
+
+@Component
+@Log4j2
+@AllArgsConstructor
+public class ScheduleService {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    private final String fileName = "d46tr32tr2.jpg";
+
+    @PostConstruct
+    public void tets(){
+        JFrame jFrame = new JFrame("Title");
+        jFrame.setVisible(false);
+    }
+
+    static {
+        System.setProperty("java.awt.headless", "false");
+    }
+
+    @Scheduled(fixedDelay = 500L)
+    public void sendImage() {
+        try {
+            final Robot robot = new Robot();
+            final File filePath = new File(fileName);
+            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
+            ImageIO.write(bufferedImage, "jpg", filePath);
+            byte[] imageBytes = Files.readAllBytes(filePath.toPath());
+            String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+            messagingTemplate.convertAndSend("/topic/ss-data", base64Image);
+        } catch (Exception e) {
+            log.error("Error occurred ", e);
+        }
+    }
+}
